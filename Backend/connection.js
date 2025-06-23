362892edb19811e33683a7147d1ca884d23955b9
RohitBiju7 <rohitbiju2001@gmail.com>
2025-06-23 15:28:55 +0530

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb+srv://username:password@cluster0.stsw4qu.mongodb.net/blogapp") // Replace with your MongoDB URI
+  .connect("mongodb+srv://rohit:rohit123@tasktracker.wdjpgcp.mongodb.net/?retryWrites=true&w=majority&appName=TaskTracker") // Replace with your MongoDB URI
   .then(() => {
     console.log("Connected to DB");
   })
